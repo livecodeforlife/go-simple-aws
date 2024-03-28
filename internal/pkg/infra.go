2a45c72efbdd7cd89df78adad1ebc67f84b9c46d
gracig <15052330+gracig@users.noreply.github.com>
2024-03-28 11:15:03 -0300

    Create the basic functionality to create components
    - Provider represents an external Cloud Provider
    - Infra is the main component to create cloud  resources
    - Resource[T] is the representation of a generated Resource
    
    100% coverage on tests

--- internal/pkg/infra.go
+++ internal/pkg/infra.go
@@ -1,18 +1,115 @@
 package infra
 
-// VPCDefinition has the configuration to create a VPC
-type VPCDefinition struct {
+import "fmt"
+
+// DefinitionStore store the definition
+type DefinitionStore interface {
+	mapInterface
 }
 
-// VPCHandle is the object that the VPC Creator returns when it creates a VPC
-type VPCHandle struct {
+// Provider implements Creator interfaces
+// External struct should implement provider to create cloud components
+type Provider interface {
+	VPCCreator
+	DNSCreator
+	SubnetCreator
+	HandlerStore
+}
+
+// HandlerStore store the handlers
+// Handlers is a abstraction about the created component at the cloud
+type HandlerStore interface {
+	mapInterface
+}
+
+// SubnetCreator is the interface that does create the VPC
+type SubnetCreator interface {
+	CreateSubnet(Subnet) (*Resource[Subnet], error)
 }
 
 // VPCCreator is the interface that does create the VPC
 type VPCCreator interface {
-	create(vpc VPCDefinition) (*VPCHandle, error)
+	CreateVPC(VPC) (*Resource[VPC], error)
 }
 
-func createOrUpdateVpc(def VPCDefinition, vpc VPCCreator) (*VPCHandle, error) {
-	return vpc.create(def)
+// DNSCreator is the interface that does create the DNSCreator
+type DNSCreator interface {
+	CreateDNS(DNS) (*Resource[DNS], error)
+}
+
+// Resource represents an infra component that was already deployed
+type Resource[T any] struct {
+	id      string
+	handler interface{} // this should be known by the provider
+}
+
+// Infra manages the creation, update and deletion of infra components
+type Infra struct {
+	provider  Provider
+	resources map[string]interface{}
+}
+
+// New returns a new infra provider
+func New(provider Provider) *Infra {
+	return &Infra{provider, make(map[string]interface{})}
+}
+
+// VPC defines a VPC to be deployed
+type VPC struct {
+}
+
+// Subnet defines a subnet in the VPC
+type Subnet struct {
+	vpc *Resource[VPC]
+}
+
+// DNS defines a DNS
+type DNS struct {
+}
+
+// CreateVPC takes a VPC resource and creates it in the cloud
+func (i *Infra) CreateVPC(id string, resourceDef VPC) (*Resource[VPC], error) {
+	return createResource[VPC](i, id, resourceDef, i.provider.CreateVPC)
+}
+
+// CreateSubnet takes a Subnet resource and creates it in the cloud
+func (i *Infra) CreateSubnet(id string, resourceDef Subnet) (*Resource[Subnet], error) {
+	return createResource[Subnet](i, id, resourceDef, i.provider.CreateSubnet)
+}
+
+// CreateDNS is a dns creator
+func (i *Infra) CreateDNS(id string, resourceDef DNS) (*Resource[DNS], error) {
+	return createResource[DNS](i, id, resourceDef, i.provider.CreateDNS)
+}
+
+// createResource provides shared functionality among the create* functions
+func createResource[T any](infra *Infra, id string, resourceDef T, create func(T) (*Resource[T], error)) (*Resource[T], error) {
+	if infra.provider == nil {
+		return nil, fmt.Errorf("provider is nil")
+	}
+	if infra.resources == nil {
+		return nil, fmt.Errorf("you must create Infra with the New function")
+	}
+	if _, ok := infra.resources[id]; ok {
+		return nil, fmt.Errorf("you cannot call createResource for the same id twice")
+	}
+	if !infra.provider.Exists(id) {
+		Resource, err := create(resourceDef)
+		if err != nil {
+			return nil, err
+		}
+		infra.provider.Set(id, Resource)
+	}
+	handler := infra.provider.Get(id)
+	if handler == nil {
+		return nil, fmt.Errorf("Provider has returned a nil handler")
+	}
+	infra.resources[id] = resourceDef
+	return handler.(*Resource[T]), nil
+}
+
+type mapInterface interface {
+	Set(key string, value interface{})
+	Get(key string) interface{}
+	Exists(key string) bool
 }
--- internal/pkg/infra_test.go
+++ internal/pkg/infra_test.go
@@ -1,57 +1,178 @@
 package infra
 
 import (
-	"reflect"
+	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-type VpcCreatorImpl struct {
-	handle VPCHandle
-	calls  uint
+type TVPCCreator struct {
+	Response *Resource[VPC]
+	Err      error
 }
 
-func (v *VpcCreatorImpl) create(vpc VPCDefinition) (*VPCHandle, error) {
-	v.calls++
-	return &v.handle, nil
+func (c *TVPCCreator) CreateVPC(r VPC) (*Resource[VPC], error) {
+	return c.Response, c.Err
 }
 
-func Test_createOrUpdateVpc(t *testing.T) {
-	type args struct {
-		def VPCDefinition
-		vpc VPCCreator
-	}
+type TDNSCreator struct {
+	Response *Resource[DNS]
+	Err      error
+}
 
-	vpcCreator := VpcCreatorImpl{
-		handle: VPCHandle{},
-		calls:  0,
-	}
+func (c *TSubnetCreator) CreateSubnet(r Subnet) (*Resource[Subnet], error) {
+	return c.Response, c.Err
+}
 
-	tests := []struct {
-		name    string
-		args    args
-		want    *VPCHandle
-		wantErr bool
-	}{
-		{
-			name: "Given a definition and a vpc creator, the function should call the create method",
-			args: args{
-				def: VPCDefinition{},
-				vpc: &vpcCreator,
-			},
-			wantErr: false,
-			want:    &vpcCreator.handle,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := createOrUpdateVpc(tt.args.def, tt.args.vpc)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("createOrUpdateVpc() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("createOrUpdateVpc() = %v, want %v", got, tt.want)
-			}
+type TSubnetCreator struct {
+	Response *Resource[Subnet]
+	Err      error
+}
+
+func (c *TDNSCreator) CreateDNS(r DNS) (*Resource[DNS], error) {
+	return c.Response, c.Err
+}
+
+type TResourceStore struct {
+	store map[string]interface{}
+}
+
+func (p *TResourceStore) Set(key string, handler interface{}) {
+	p.store[key] = handler
+}
+func (p *TResourceStore) Get(key string) interface{} {
+	return p.store[key]
+}
+func (p *TResourceStore) Exists(key string) bool {
+	_, ok := p.store[key]
+	return ok
+}
+
+type TestProvider struct {
+	TVPCCreator
+	TDNSCreator
+	TSubnetCreator
+	TResourceStore
+}
+
+func TestCreateResource(t *testing.T) {
+	{
+		infra := &Infra{
+			provider:  nil,
+			resources: nil,
+		}
+		_, err := createResource(infra, "id", "def", func(string) (*Resource[string], error) {
+			return &Resource[string]{id: "id", handler: "handler"}, nil
 		})
+		assert.NotNil(t, err)
 	}
+	{
+		infra := &Infra{
+			provider: &TestProvider{
+				TResourceStore: TResourceStore{
+					store: make(map[string]interface{}),
+				},
+			},
+			resources: nil,
+		}
+		_, err := createResource(infra, "id", "def", func(string) (*Resource[string], error) {
+			return &Resource[string]{id: "id", handler: "handler"}, nil
+		})
+		assert.NotNil(t, err)
+	}
+	{
+		//Given a right initialized infra
+		provider := &TestProvider{
+			TResourceStore: TResourceStore{
+				store: make(map[string]interface{}),
+			},
+		}
+		infra := New(provider)
+		resource, err := createResource(infra, "id", "def", func(string) (*Resource[string], error) {
+			return &Resource[string]{id: "id", handler: "handler"}, nil
+		})
+		assert.Equal(t, resource.id, "id")
+		assert.Equal(t, resource.handler, "handler")
+		assert.Nil(t, err)
+	}
+	{
+		//Test with same id
+		infra := &Infra{
+			provider: &TestProvider{
+				TResourceStore: TResourceStore{
+					store: make(map[string]interface{}),
+				},
+			},
+			resources: map[string]interface{}{
+				"id": "id",
+			},
+		}
+		_, err := createResource(infra, "id", "def", func(string) (*Resource[string], error) {
+			return &Resource[string]{id: "id", handler: "handler"}, nil
+		})
+		assert.NotNil(t, err)
+	}
+	{
+		//When provider returns nil
+		provider := &TestProvider{
+			TResourceStore: TResourceStore{
+				store: map[string]interface{}{
+					"id": nil,
+				},
+			},
+		}
+		infra := New(provider)
+		_, err := createResource(infra, "id", "def", func(string) (*Resource[string], error) {
+			return &Resource[string]{id: "id", handler: "handler"}, nil
+		})
+		assert.NotNil(t, err)
+	}
+
+	{
+		//When provider returns error on creation
+		provider := &TestProvider{
+			TResourceStore: TResourceStore{
+				store: make(map[string]interface{}),
+			},
+		}
+		infra := New(provider)
+		_, err := createResource(infra, "id", "def", func(string) (*Resource[string], error) {
+			return nil, fmt.Errorf("Error on creation")
+		})
+		assert.Equal(t, fmt.Sprintf("%s", err), "Error on creation")
+	}
+
+}
+
+func TestCreate(t *testing.T) {
+	infra := New(&TestProvider{
+		TResourceStore: TResourceStore{
+			store: make(map[string]interface{}),
+		},
+		TVPCCreator: TVPCCreator{
+			Response: &Resource[VPC]{id: "myvpc", handler: "handler"},
+		},
+		TSubnetCreator: TSubnetCreator{
+			Response: &Resource[Subnet]{id: "mysubnet", handler: "handler"},
+		},
+		TDNSCreator: TDNSCreator{
+			Response: &Resource[DNS]{id: "mydns", handler: "handler"},
+		},
+	})
+	vpc, err := infra.CreateVPC("myvpc", VPC{})
+	assert.Equal(t, vpc.id, infra.provider.(*TestProvider).TVPCCreator.Response.id)
+	assert.Equal(t, vpc.handler, infra.provider.(*TestProvider).TVPCCreator.Response.handler)
+	assert.Equal(t, err, infra.provider.(*TestProvider).TVPCCreator.Err)
+
+	subnet, err := infra.CreateSubnet("mysubnet", Subnet{})
+	assert.Equal(t, subnet.id, infra.provider.(*TestProvider).TSubnetCreator.Response.id)
+	assert.Equal(t, subnet.handler, infra.provider.(*TestProvider).TSubnetCreator.Response.handler)
+	assert.Equal(t, err, infra.provider.(*TestProvider).TSubnetCreator.Err)
+
+	dns, err := infra.CreateDNS("mydns", DNS{})
+	assert.Equal(t, dns.id, infra.provider.(*TestProvider).TDNSCreator.Response.id)
+	assert.Equal(t, dns.handler, infra.provider.(*TestProvider).TDNSCreator.Response.handler)
+	assert.Equal(t, err, infra.provider.(*TestProvider).TDNSCreator.Err)
+
 }
